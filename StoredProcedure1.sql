0437f0eacbaaa3a067250364e22432819fd0c7cd
bhosaleanagha <55074591+bhosaleanagha@users.noreply.github.com>
2020-02-28 03:06:32 -0500

    Update StoredProcedure1.sql

--- StoredProcedure1.sql
+++ StoredProcedure1.sql
@@ -2,7 +2,7 @@ drop procedure if exists getUnansweredQuestions;
 DELIMITER //
 create procedure getUnansweredQuestions()
 BEGIN
-select q2.text as quest, q1.text, q1.answerscount, q2.module from (select a.id,a.questionid,a.text,a.upvotes,count(id) as answerscount from answer a group by questionid having max(correctanswer)=0 and min(correctanswer)=0) q1 join question q2 on q1.questionid = q2.id
+select q2.text as quest, q1.answerscount, q2.module from (select a.id,a.questionid,a.text,a.upvotes,count(id) as answerscount from answer a group by questionid having max(correctanswer)=0 and min(correctanswer)=0) q1 join question q2 on q1.questionid = q2.id
 group by q2.module having max(q1.answerscount);
 END //
 
